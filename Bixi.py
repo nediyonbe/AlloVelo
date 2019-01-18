fc7814987522c4408d48c021dbabd40d029c1e03
nediyonbe <gurkanali@gmail.com>
2019-01-18 17:46:19 -0500

    a

---
+++ Bixi.py
@@ -0,0 +1,57 @@
+# In this code I look into the demand for the Bixi bike sharing in Montreal
+
+#%%
+print('test')
+import pandas as pd
+import glob
+
+# Import bicyle use data
+#%%
+path ='C:/Users/Ali/Documents/Insight/Bixi/Trips' # use your path
+all_files = glob.glob(path + "/*.csv")
+
+#%%
+listy = []
+
+#%%
+counter_f = 0
+for f in all_files:
+    print(f)
+    col_names = ['start_date', 'start_station_code', 'end_date', 'end_station_code', 'duration_sec', 'is_member']
+    df = pd.read_csv(f, skiprows = 1, names = col_names)
+    listy.append(df)
+
+#%%
+df_trips = pd.concat(listy, axis = 0, ignore_index = True)
+df_trips.describe()
+
+#Check data types, datetime fields may not be
+#%%
+df_trips.dtypes
+
+#Set related fields to datetime
+#%%
+df_trips['start_date'] = pd.to_datetime(df_trips['start_date'])
+df_trips['end_date'] = pd.to_datetime(df_trips['end_date'])
+
+#Get year and month info in separate fields. This can be useful for analyses
+#%%
+df_trips['start_year'] = pd.DatetimeIndex(df_trips['start_date']).year
+df_trips['start_month'] = pd.DatetimeIndex(df_trips['start_date']).month
+
+# Import weather data
+f_weather ='C:/Users/Ali/Documents/Insight/Bixi/Weather Data/eng-hourly-01012014-18-01-2019.csv' # use your path
+col_names_weather = ['date_time_local', 'unixtime',	'pressure_station',	'pressure_sea',	'wind_dir',	'wind_dir_10s',	'wind_speed', 'wind_gust', 'relative_humidity', 'dew_point', 'temperature', 'windchill', 'humidex', 'visibility', 'health_index',	'cloud_cover_4', 'cloud_cover_8', 'cloud_cover_10', 'solar_radiation']
+df_weather = pd.read_csv(f_weather, skiprows = 1, names = col_names_weather)
+
+#%%
+for_plot = df_trips.groupby('start_month').size()
+for_plot
+
+#%%
+import seaborn as sns
+ax = sns.barplot(data = for_plot)
+
+#%%
+label_x = ["Apr","May","Jun","Jul","Aug","Sep","Oct","Nov"]
+ax = x.plot.bar(x='label_x', y = 'for_plot', rot = 0)
